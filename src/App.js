68210df431784934b3184c4c8e5b32cfa5f331a7
Ciprian <cozma_ciprian19@yahoo.com>
2021-07-04 18:39:52 +0300

    useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import { useFetch } from './useFetch';
 import { useForm } from './useForm';
 
 function App() {
 
-    const [values, handleChange] = useForm({email: "", password: ""})
+    const [values, handleChange] = useForm({email: "", password: "", firstName: ""})
 
     const [count, setCount] = useState(() => JSON.parse(localStorage.getItem("count")));
 
    const {data} =  useFetch(`http://numbersapi.com/${count}/trivia`);
+
+   const inputRef = useRef();
 
    useEffect(() => {
        localStorage.setItem("count", JSON.stringify(count))
@@ -21,16 +23,24 @@ function App() {
     <div>{count}</div>
 
     <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
+    <input 
+        name="first name"
+        value={values.firstName}
+        onChange={handleChange}
+        placeholder="first name"
+        ref={inputRef}
+     />
      <input 
         name="email"
         value={values.email}
         onChange={handleChange}
      />
-      <input 
+      <input
         name="password"
         value={values.password}
         onChange={handleChange}
      />
+     <button onClick={() => inputRef.current.focus()}>Focus</button>
     </div>
   );
 }
